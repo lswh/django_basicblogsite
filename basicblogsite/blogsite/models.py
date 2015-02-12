fa46784c9d4b9391d32e2735114940731974e256
heleyna <labao.hm@gmail.com>
2015-02-12 08:34:20 +0800

    defined basic models here and populated essentials for blog entry fields

--- basicblogsite/blogsite/models.py
+++ basicblogsite/blogsite/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# Blog Main
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+# Blog Post
+class Blogpost(models.Model):
+	blog = models.ForeignKey(Blog)
+	posttitle = models.CharField(max_length=255)
+	bodytext = models.TextField()
+	pub_date = models.DateField()
+	mood = models.CharField(max_length=30)
+	tags = models.TextField()
+    # music = models.
+    # latitude = models.
+    # longitude = models.
+    # numberofcomments = models.IntegerField()
+    #social media shares
+
+# To Do List
+# class Todolist(models.Model):
+
+# Event Posting	
+# class Events(models.Model):
+
