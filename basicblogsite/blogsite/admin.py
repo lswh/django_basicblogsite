04b4ff04f2e3f5677c141db5067c808bb46e4e49
heleyna <labao.hm@gmail.com>
2015-02-12 08:45:57 +0800

    Added ability to edit multiple blogs on Admin interface

--- basicblogsite/blogsite/admin.py
+++ basicblogsite/blogsite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blogsite.models import Blogpost
+from blogsite.models import Blogpost,Blog
 
 # Register your models here.
 admin.site.register(Blogpost)
+admin.site.register(Blog)
