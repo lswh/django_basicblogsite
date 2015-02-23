9705ed9eac86e66d6ceea371823f3e9dcb2653de
heleyna <labao.hm@gmail.com>
2015-02-23 16:03:54 +0800

    defined unicode self for the two model classes

--- basicblogsite/basicblogsite/urls.py
+++ basicblogsite/basicblogsite/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'basicblogsite.views.home', name='home'),
+    #url(r'^$', 'basicblogsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^blogsite/', include('blogsite.urls')),
     url(r'^admin/', include(admin.site.urls)),
--- basicblogsite/blogsite/models.py
+++ basicblogsite/blogsite/models.py
@@ -7,19 +7,27 @@ class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
 
+    def __unicode__(self):
+        return self.name
+
 # Blog Post
 class Blogpost(models.Model):
-	blog = models.ForeignKey(Blog)
-	posttitle = models.CharField(max_length=255)
-	bodytext = models.TextField()
-	pub_date = models.DateField()
-	mood = models.CharField(max_length=30)
-	tags = models.TextField()
-    # music = models.
+    blog = models.ForeignKey(Blog)
+    posttitle = models.CharField(max_length=255)
+    bodytext = models.TextField()
+    pub_date = models.DateField()
+    mood = models.CharField(max_length=30)
+    tags = models.TextField()
+    
+    def __unicode__(self):
+        return self.posttitle
+
+# music = models.
     # latitude = models.
     # longitude = models.
     # numberofcomments = models.IntegerField()
     #social media shares
+    
 
 # To Do List
 # class Todolist(models.Model):
