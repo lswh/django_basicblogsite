e4aea01155e8f021cd4b74728967c045948a3690
heleyna <labao.hm@gmail.com>
2015-02-25 09:33:53 +0800

    reflected blog post objects in the homepage of blogpost

--- basicblogsite/blogsite/views.py
+++ basicblogsite/blogsite/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blogsite.models import Blog,Blogpost
+
 # Create your views here.
 def homepage(request):
-	return HttpResponse("Bonjour et Bienvenue!</p>This is a test homepage view until I learn frontend to make it pretty.")
+	#return HttpResponse("Bonjour et Bienvenue!</p>This is a test homepage view until I learn frontend to make it pretty.")
+	posts = Blogpost.objects.order_by('-pub_date')[:5]
+	output = ', '.join([p.posttitle for p in posts])
+	return HttpResponse(output)
 
 #def singlepost(request):
 #	return 
